a6181b6c581987e4110aa1eb7b0e984220314551
yusuke <tigers14@ezweb.ne.jp>
2021-06-30 22:35:57 +0900

    all commit after complete

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
+import './App.css';
+
+function App() {
+
+  const [city, setCity] = useState("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
+  const getWeather = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=8373ce37cc3a4b3c99870819213006&q=${city}&aqi=no`)
+    .then(res => {
+      setResults({
+        country: res.data.location.country,
+        cityName: res.data.location.name,
+        temperature: res.data.current.temp_c,
+        conditionText: res.data.current.condition.text,
+        icon: res.data.current.condition.icon
+      })
+    })
+  }
+
+  return (
+    <div className="Wrapper">
+      <div class="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1>世界の天気</h1>
+  );  
+};
+
+export default Title;
