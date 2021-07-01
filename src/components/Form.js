ee3fc6f3377c48efc1f01c78cf36a8dbb5694a20
yusuke <tigers14@ezweb.ne.jp>
2021-07-01 11:23:30 +0900

    destructuring assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
 
 
-const Form = (props) => {
+const Form = ({ setCity, getWeather }) => {
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
-      <button type="submit" onClick={props.getWeather}>お天気情報</button>
+      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+      <button type="submit" onClick={getWeather}>お天気情報</button>
     </form>
   );
 };
