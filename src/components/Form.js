d360dde7029e805a8fc281ee040b84c5e83c805f
yusuke <tigers14@ezweb.ne.jp>
2021-07-01 17:51:13 +0900

    fixed the error

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@
 const Form = ({ city, setCity, getWeather }) => {
   return (
     <form onSubmit={getWeather}>
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value) value={city}}/>
+      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city}/>
       <button type="submit" onClick={getWeather}>お天気情報</button>
     </form>
   );
