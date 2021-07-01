3788c9863945fd120f3c86f9c4fa8c5f0e1d8de4
yusuke <tigers14@ezweb.ne.jp>
2021-07-01 14:00:24 +0900

    prevent unintended behavior

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@
 
 const Form = ({ setCity, getWeather }) => {
   return (
-    <form>
+    <form onSubmit={getWeather}>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-      <button type="submit" onClick={getWeather}>お天気情報</button>
+      <button type="submit">お天気情報</button>
     </form>
   );
 };
